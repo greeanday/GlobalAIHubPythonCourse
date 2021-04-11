7720263ad5e6e25482cb31d6fc4b56b2ae049782
greeanday <81861276+greeanday@users.noreply.github.com>
2021-04-11 22:17:24 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -13,3 +13,20 @@ elif Password==Kul_pas:
     print("kullanıcı adını yanlış girdiniz !")
 else:
     print("kullanıcı adı ve şifre hatalı !")
+    
+#Extra:
+Username="şeyma"
+Password=4929
+Kul_us=input("Username: ")
+Kul_pas=int(input("Password: "))
+
+s={}
+s[Username]=Kul_us
+s[Password]=Kul_pas
+
+
+if list(s.keys())==list(s.values()):
+    print("kullanıcı adı ve şifre doğru !")
+else:
+    print("hatalı giriş yaptınız !")
+
