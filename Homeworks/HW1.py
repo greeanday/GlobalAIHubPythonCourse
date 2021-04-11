0c36c3c0e397fd0df352858c573986184599e112
greeanday <81861276+greeanday@users.noreply.github.com>
2021-04-11 21:53:40 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+liste=["a","b","c","d","e","f"]
+k=0
+s=int(len(liste)/2)
+
+while k<len(liste)/2:
+    
+    liste[k],liste[s]=liste[s],liste[k]
+    k+=1
+    s+=1
+    
+print(liste)
+
+#Question 2
+n=int(input("lütfen tek basamaklı bir tamsayı giriniz: "))
+l=[i for i in range(n+1) if i%2==0]
+print(l)
